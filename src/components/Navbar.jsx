199b84e4324583e043dfdfe294d13c110759dc8b
Daniel C <68665806+dchavez3395@users.noreply.github.com>
2022-11-03 00:09:46 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
           <h2 className='hover:underline text-xl font-bold underline-offset-4 cursor-pointer'> Menu</h2>
           <div className='flex flex-row group'>
             <div className='p-[10px] bg-white lg:h-[50px] h-[40px] w-[40px] lg:w-[50px] rounded-full'>
-              <img src="/../src/assets/telephone.png" alt="phone-img" className='h-[32px]w-[32px] transitions-theme group-hover:-rotate-[360deg] transition-all duration-[1000ms] ease-in-out cursor-pointer' />
+              <img src="https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/telephone.png" alt="phone-img" className='h-[32px]w-[32px] transitions-theme group-hover:-rotate-[360deg] transition-all duration-[1000ms] ease-in-out cursor-pointer' />
             </div>
             <div className='text-white ml-[20px]'>
               <div className='font-normal lg:text-md'>ORDER NOW</div>
@@ -42,7 +42,7 @@ const Navbar = () => {
 
         <div className='pt-3'>
           <button type='button' onClick={onCartToggle} className='cursor-pointer'>
-            <img src="/../src/assets/cart.png" className='lg:h-[32px] h-[25px] w-[25px] lg:w-[32px]' alt="" />
+            <img src="https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/cart.png" className='lg:h-[32px] h-[25px] w-[25px] lg:w-[32px]' alt="" />
             <div className={`font-medium absolute top-6 lg:top-6 lg:right-9 right-5 text-[#d1411e] bg-white  shadow lg:w-4 lg:h-4 w-3.5 h-3.5 text-xs lg:text-[0.75rem] leading-tight rounded-full items-center flex justify-center cursor-pointer transition-all duration-300 hover:scale-110`}>{totalQTY}</div>
           </button>
         </div>
