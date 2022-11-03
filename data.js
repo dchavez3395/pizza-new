9d870fd995d1d14ea81d285a5b0839e536d0f390
Daniel C <68665806+dchavez3395@users.noreply.github.com>
2022-11-03 00:08:06 -0400

    Update data.js

--- data.js
+++ data.js
@@ -2,56 +2,56 @@ export const pizzas = [
     {
         id: '1',
         name: 'PEPPERONI LOVERS',
-        image: '/../src/assets/pizzas/pizza1.png',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/pizzas/pizza1.png',
         price: [13.95, 20.49, 26.83],
         description: 'Doble pepperoni and extra high quality mozarella'
     },
     {
         id: '2',
         name: 'GARDEN CHICKEN',
-        image: '/../src/assets/pizzas/pizza2.png',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/pizzas/pizza2.png',
         price: [11.95, 20.49, 26.83],
         description: 'oven-roasted chicken, and high quality veggie blend.'
     },
     {
         id: '3',
         name: 'HAM MUSHROOM',
-        image: '/../src/assets/pizzas/pizza3.png',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/pizzas/pizza3.png',
         price: [12.95, 20.49, 26.83],
         description: 'thin sliced ham, mushroom and mozarella '
     },
     {
         id: '4',
         name: 'SPICY HAM AND PEP',
-        image: '/../src/assets/pizzas/pizza4.png',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/pizzas/pizza4.png',
         price: [15.95, 20.49, 26.83],
         description: 'Pepperoni, ham, Jalapeño and mozarella'
     },
     {
         id: '5',
         name: 'DELUXE VEGGIE LOVERS',
-        image: '/../src/assets/pizzas/pizza5.png',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/pizzas/pizza5.png',
         price: [14.95, 20.49, 26.83],
         description: 'Green/red peppers, onion, olives and mushrooms'
     },
     {
         id: '6',
         name: 'CHEESE LOVERS',
-        image: '/../src/assets/pizzas/pizza6.png',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/pizzas/pizza6.png',
         price: [12.95, 20.49, 26.83],
         description: 'Sharp cheddar, whole milk mozarella, and Parmesan'
     },
     {
         id: '7',
         name: 'ITALIAN MUSHROOM',
-        image: '/../src/assets/pizzas/pizza7.png',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/pizzas/pizza7.png',
         price: [15.95, 20.49, 26.83],
         description: 'Spicy italian sausage, mushroom and mozarella'
     },
     {
         id: '8',
         name: 'MEDITERANIAN ',
-        image: '/../src/assets/pizzas/pizza8.png',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/pizzas/pizza8.png',
         price: [14.95, 20.49, 26.83],
         description: 'Red onion pepper mix, olives mushrooms and tzatziki'
     },
@@ -61,63 +61,63 @@ export const starters = [
     {
         id: '1',
         name: 'Breadsticks',
-        image: '/../src/assets/starters/breadsticks.jpg',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/starters/breadsticks.jpg',
         price: '5.99',
         description: 'Crispy on the outside, soft on the inside'
     },
     {
         id: '2',
         name: 'Cheese Bread',
-        image: '/../src/assets/starters/cheese-bread.png',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/starters/cheese-bread.png',
         price: '7.99',
         description: 'A blend of cheddar and mozzarella cheeses baked to perfection.'
     },
     {
         id: '3',
         name: 'Mozza Sticks',
-        image: '/../src/assets/starters/mozza-sticks.jpg',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/starters/mozza-sticks.jpg',
         price: '9.99',
         description: 'Loaded with real gooey mozzarella and a crispy breading that delivers a big CRUNCH.'
     },
     {
         id: '4',
         name: 'Boneless Wings',
-        image: '/../src/assets/starters/boneless-wings.jpg',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/starters/boneless-wings.jpg',
         price: '12.90',
         description: 'Crispy, breaded seasoned chicken breast pieces tossed in your choice of sauce.'
     },
     {
         id: '5',
         name: 'Traditional Wings',
-        image: '/../src/assets/starters/traditional-wings.jpg',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/starters/traditional-wings.jpg',
         price: '12.95',
         description: 'Classic chicken wings tossed in your choice of sauce.'
     },
     {
         id: '6',
         name: 'Caesar Salad',
-        image: '/../src/assets/starters/ceasar-salad.jpg',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/starters/ceasar-salad.jpg',
         price: '6.99',
         description: 'Romaine lettuce, bacon strips, croutons and parmesan.'
     },
     {
         id: '7',
         name: 'Chicken Caesar ',
-        image: '/../src/assets/starters/chicken-ceasar-salad.jpg',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/starters/chicken-ceasar-salad.jpg',
         price: '8.99',
         description: 'grilled chicken, romaine lettuce, bacon and cheese.'
     },
     {
         id: '8',
         name: 'French Fries',
-        image: '/../src/assets/starters/fries.jpg',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/starters/fries.jpg',
         price: '11.50',
         description: 'Crispy golden fries, fried to perfection.'
     },
     {
         id: '9',
         name: 'Parm Bites',
-        image: '/../src/assets/starters/parm-bites.jpg',
+        image: 'https://raw.githubusercontent.com/dchavez3395/pizza-new/main/src/assets/starters/parm-bites.jpg',
         price: '10.56',
         description: 'Bite-size breadsticks seasoned with garlic and Parmesan.'
     },
